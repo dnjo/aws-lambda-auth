d5ce28000937b1d5dac16f301843dafa57dddd4f
Dennis Johansson <dennis@dnjo.net>
2019-01-01 17:23:59 +0100

    Initial commit

--- index.js
+++ index.js
@@ -1,60 +1,90 @@
-var https = require('https');
-var jose = require('node-jose');
+const https = require('https');
+const jose = require('node-jose');
 
-var region = 'ap-southeast-2';
-var userpool_id = 'ap-southeast-2_xxxxxxxxx';
-var app_client_id = '<ENTER APP CLIENT ID HERE>';
-var keys_url = 'https://cognito-idp.' + region + '.amazonaws.com/' + userpool_id + '/.well-known/jwks.json';
+const region = process.env['AWS_REGION'];
+const userpool_id = process.env['USERPOOL_ID'];
+const app_client_id = process.env['APP_CLIENT_ID'];
+const keys_url = 'https://cognito-idp.' + region + '.amazonaws.com/' + userpool_id + '/.well-known/jwks.json';
+
+const getJwk = () => {
+    return new Promise((resolve, reject) => {
+        https.get(keys_url, (response) => {
+            if (response.statusCode === 200) {
+                response.on('data', body => resolve(body));
+            } else {
+                reject('Invalid response code');
+            }
+
+            response.on('error', error => reject(error));
+        })
+    })
+};
+
+const verifyJwt = (token, kid, jwkBody) => {
+    return new Promise((resolve, reject) => {
+        const keys = JSON.parse(jwkBody)['keys'];
+        // search for the kid in the downloaded public keys
+        let key_index = -1;
+        for (let i = 0; i < keys.length; i++) {
+            if (kid === keys[i].kid) {
+                key_index = i;
+                break;
+            }
+        }
+        if (key_index === -1) {
+            console.log('Public key not found in jwks.json');
+            reject('Public key not found in jwks.json');
+        }
+        // construct the public key
+        jose.JWK.asKey(keys[key_index])
+          .then(result => jose.JWS.createVerify(result).verify(token))
+          .then(result => {
+              // now we can use the claims
+              const claims = JSON.parse(result.payload);
+              // additionally we can verify the token expiration
+              const current_ts = Math.floor(new Date() / 1000);
+              if (current_ts > claims.exp) {
+                  reject('Token is expired');
+              }
+              // and the Audience (use claims.client_id if verifying an access token)
+              if (claims.aud !== app_client_id) {
+                  reject('Token was not issued for this audience');
+              }
+              resolve(claims);
+          })
+          .catch(error => reject(error));
+    });
+};
+
+const generatePolicy = (principalId, effect, resource) => {
+  const authResponse = {};
+
+  authResponse.principalId = principalId;
+  if (effect && resource) {
+    const policyDocument = {};
+    policyDocument.Version = '2012-10-17';
+    policyDocument.Statement = [];
+    const statementOne = {};
+    statementOne.Action = 'execute-api:Invoke';
+    statementOne.Effect = effect;
+    statementOne.Resource = resource;
+    policyDocument.Statement[0] = statementOne;
+    authResponse.policyDocument = policyDocument;
+  }
+
+  return authResponse;
+};
 
 exports.handler = (event, context, callback) => {
-    var token = event.token;
-    var sections = token.split('.');
+    const token = event.authorizationToken;
+    const sections = token.split('.');
     // get the kid from the headers prior to verification
-    var header = jose.util.base64url.decode(sections[0]);
-    header = JSON.parse(header);
-    var kid = header.kid;
-    // download the public keys
-    https.get(keys_url, function(response) {
-        if (response.statusCode == 200) {
-            response.on('data', function(body) {
-                var keys = JSON.parse(body)['keys'];
-                // search for the kid in the downloaded public keys
-                var key_index = -1;
-                for (var i=0; i < keys.length; i++) {
-                        if (kid == keys[i].kid) {
-                            key_index = i;
-                            break;
-                        }
-                }
-                if (key_index == -1) {
-                    console.log('Public key not found in jwks.json');
-                    callback('Public key not found in jwks.json');
-                }
-                // construct the public key
-                jose.JWK.asKey(keys[key_index]).
-                then(function(result) {
-                    // verify the signature
-                    jose.JWS.createVerify(result).
-                    verify(token).
-                    then(function(result) {
-                        // now we can use the claims
-                        var claims = JSON.parse(result.payload);
-                        // additionally we can verify the token expiration
-                        var current_ts = Math.floor(new Date() / 1000);
-                        if (current_ts > claims.exp) {
-                            callback('Token is expired');
-                        }
-                        // and the Audience (use claims.client_id if verifying an access token)
-                        if (claims.aud != app_client_id) {
-                            callback('Token was not issued for this audience');
-                        }
-                        callback(null, claims);
-                    }).
-                    catch(function() {
-                        callback('Signature verification failed');
-                    });
-                });
-            });
-        }
-    });
-}
+    const header = JSON.parse(jose.util.base64url.decode(sections[0]));
+    getJwk()
+      .then(jwkBody => verifyJwt(token, header.kid, jwkBody))
+      .then(claims => {
+        const policyResponse = generatePolicy(claims.sub, 'Allow', event.methodArn);
+        callback(null, policyResponse);
+      })
+      .catch(callback('Invalid token'));
+};
